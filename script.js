6b93b0881923afbf06e27c16371034e61bd76789
Costanza <60357882+costikid@users.noreply.github.com>
2022-07-18 13:48:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const menuToggle = document.querySelector('.toggle');
-const showcase = document.querySelector('.showcase');
 
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
-  showcase.classList.toggle('active');
-})
 
+
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
