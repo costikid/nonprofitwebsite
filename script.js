e2da274094d7216af2d580e880c2c00055f907f6
Costanza <60357882+costikid@users.noreply.github.com>
2023-06-19 20:30:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 
+// modal opens when we click on image
 
+const galleryImages = document.querySelectorAll('.gallery__image');
+const modal = document.getElementById('modal');
+const modalImage = document.querySelector('.modal__image');
+const modalClose = document.querySelector('.modal__close');
 
+galleryImages.forEach((image) => {
+  image.addEventListener('click', () => {
+    modal.style.display = 'flex';
+    modalImage.src = image.src;
+  });
+});
+
+modalClose.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
 window.onscroll = function() {myFunction()};
 
 // Get the navbar
@@ -17,3 +32,8 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+
+
+
+
